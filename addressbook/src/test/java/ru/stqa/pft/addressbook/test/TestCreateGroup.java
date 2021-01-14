0b4fb6418d66ead707b70de679eafe66321571e4
MARKOmcPOLO <77144932+MARKOmcPOLO@users.noreply.github.com>
2021-01-14 14:42:11 +0600

    Add Helpers

--- addressbook/src/test/java/ru/stqa/pft/addressbook/test/TestCreateGroup.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/test/TestCreateGroup.java
@@ -7,11 +7,11 @@ public class TestCreateGroup extends TestBase {
 
     @Test
     public void testCreateGroupMethod() throws Exception {
-        app.gotoGroupPage();
-        app.initGroupCreation();
-        app.fillGroupForm(new GroupData("test1", "test1", "test1"));
-        app.submitGroupCreation();
-        app.returnToGroupPage();
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test1", "test1"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
     }
 
 }
